35b3c601feb3a1255220f2e5a8cf456db7045af8
Henry Backman <henry.backman@camptocamp.com>
2026-05-21 18:55:25 +0200

    fixup! [ADD] stock_picking_delivery_package_type_carrier

--- stock_picking_delivery_package_type_carrier/models/stock_quant_package.py
+++ stock_picking_delivery_package_type_carrier/models/stock_quant_package.py
@@ -11,31 +11,27 @@ class StockQuantPackage(models.Model):
         help="Technical field. Result move lines for this package.",
     )
 
-    def _check_package_type_carrier_compatibility(self, package_type):
-        dedicated_carriers = package_type.package_carrier_ids
-        if not dedicated_carriers:
-            return
-
-        for package in self:
-            package_carriers = package.result_move_line_ids.mapped(
-                "picking_id.carrier_id"
-            )
-            if not package_carriers:
-                continue
-            if not any(carrier in dedicated_carriers for carrier in package_carriers):
-                raise ValidationError(
-                    _(
-                        "Package type '%(package_type)s' is not valid for carrier"
-                        " '%(carrier)s'."
-                    )
-                    % {
-                        "package_type": package_type.name,
-                        "carrier": package_carriers[0].name,
-                    }
-                )
-
     @api.constrains("package_type_id")
     def _check_package_type_id_carrier_compatibility(self):
         for package in self:
-            if package_type := package.package_type_id:
-                package._check_package_type_carrier_compatibility(package_type)
+            package_type = package.package_type_id
+            if not package_type:
+                continue
+            dedicated_carriers = package_type.package_carrier_ids
+            if not dedicated_carriers:
+                continue
+            package_carriers = package.result_move_line_ids.filtered(
+                lambda ml: ml.state in ("partially_available", "assigned")
+            ).picking_id.carrier_id
+            if not package_carriers:
+                continue
+            if not all(carrier in dedicated_carriers for carrier in package_carriers):
+                carrier_names = ", ".join(carrier.name for carrier in package_carriers)
+                msg = _(
+                    "Package type '%(package_type)s' is not valid for carriers"
+                    " '%(carrier)s'."
+                ) % {
+                    "package_type": package_type.name,
+                    "carrier": carrier_names,
+                }
+                raise ValidationError(msg)
--- stock_picking_delivery_package_type_carrier/tests/test_stock_quant_package.py
+++ stock_picking_delivery_package_type_carrier/tests/test_stock_quant_package.py
@@ -62,3 +62,48 @@ class TestStockQuantPackageConstraint(CommonChooseDeliveryPackage, BaseCommon):
 
         with self.assertRaises(ValidationError):
             package.write({"package_type_id": package_type.id})
+
+    def test_constraint_rejects_package_type_when_not_valid_for_all_carriers(self):
+        package, picking = self._create_package_for_picking_carrier()
+        carrier_a = picking.carrier_id
+
+        carrier_b = self.env["delivery.carrier"].create(
+            {
+                "name": "Carrier B",
+                "delivery_type": carrier_a.delivery_type,
+                "product_id": self.product_delivery.id,
+            }
+        )
+
+        # create a second picking with carrier B and link the same package
+        picking2 = self.env["stock.picking"].create(
+            {
+                "picking_type_id": picking.picking_type_id.id,
+                "location_id": self.env.ref("stock.stock_location_stock").id,
+                "location_dest_id": self.env.ref("stock.stock_location_customers").id,
+                "carrier_id": carrier_b.id,
+            }
+        )
+        move_line2 = self.env["stock.move.line"].create(
+            {
+                "product_id": self.product.id,
+                "product_uom_id": self.product.uom_id.id,
+                "picking_id": picking2.id,
+                "quantity": 1,
+                "location_id": self.env.ref("stock.stock_location_stock").id,
+                "location_dest_id": self.env.ref("stock.stock_location_customers").id,
+            }
+        )
+        move_line2.result_package_id = package
+
+        # package type is dedicated to carrier_a only, not carrier B
+        package_type = self.env["stock.package.type"].create(
+            {
+                "name": "Carrier A Only",
+                "package_carrier_type": carrier_a.delivery_type,
+                "package_carrier_ids": [Command.set([carrier_a.id])],
+            }
+        )
+
+        with self.assertRaises(ValidationError):
+            package.write({"package_type_id": package_type.id})
